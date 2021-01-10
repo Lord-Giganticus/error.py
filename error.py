abeb6dc749edde165518f43ce5914b9d13f95239
Lord-Giganticus <mrubins64@gmail.com>
2021-01-10 11:44:29 -0600

    Update print functions

--- error.py
+++ error.py
@@ -2,10 +2,10 @@
 class error:
   def problem(x:int, y:int, z:str):
     if y == 0:
-      print("There was a input error on line",str(x),' of file',str(z)+'.')
+      print("There was a input error on line",str(x),'of file',str(z)+'.')
     elif y != 0 and y > 0:
-      print('There was a error somewhere between line',str(x),'and line',str(y),' of file',str(z)+'.')
+      print('There was a error somewhere between line',str(x),'and line',str(y),'of file',str(z)+'.')
     elif y != 0 and y < 0:
-      print('y is less than 0! It is set to',str(y),'the starting line is line',str(x),' of file',str(z)+'. Please inform the author that this has occured!')
+      print('y is less than 0! It is set to',str(y)+'. the starting line is line',str(x),'of file',str(z)+'. Please inform the author that this has occured!')
     input('Press enter to exit.')
     exit()
